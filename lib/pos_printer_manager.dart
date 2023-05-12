6f285cc9c57293fa6ebc931322af70c13b1f886c
Elliot Tabraham-Dowers <elta@raceresult.com>
2023-05-12 14:07:00 +0800

    git branch of esc_pos_utils

--- lib/pos_printer_manager.dart
+++ lib/pos_printer_manager.dart
@@ -9,18 +9,13 @@ export 'package:pos_printer_manager/services/usb_printer_manager.dart';
 export 'package:pos_printer_manager/enums/bluetooth_printer_type.dart';
 export 'package:pos_printer_manager/enums/connection_response.dart';
 export 'package:pos_printer_manager/enums/connection_type.dart';
-export 'package:esc_pos_utils_plus/esc_pos_utils.dart';
+export 'package:esc_pos_utils/esc_pos_utils.dart';
 
 class PosPrinterManager {
   static EasyLogger logger = EasyLogger(
     name: 'pos_printer_manager',
     defaultLevel: LevelMessages.debug,
     enableBuildModes: [BuildMode.debug, BuildMode.profile, BuildMode.release],
-    enableLevels: [
-      LevelMessages.debug,
-      LevelMessages.info,
-      LevelMessages.error,
-      LevelMessages.warning
-    ],
+    enableLevels: [LevelMessages.debug, LevelMessages.info, LevelMessages.error, LevelMessages.warning],
   );
 }
